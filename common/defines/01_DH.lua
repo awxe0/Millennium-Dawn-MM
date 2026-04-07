ec8f440b547f28a40d8033eae9c69f6ddbc97099
awxe <alialsancak37@gmail.com>
2026-04-07 04:28:04 +0300

    Dark Map

---
+++ common/defines/01_DH.lua
@@ -0,0 +1,3 @@
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_HUE_MODIFIER = 0.0 -- 0.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.8 -- 0.6
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.75 -- 0.8
