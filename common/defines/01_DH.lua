67fd39c6f2ecf781102e50135595f7e7379298e2
awxe <alialsancak37@gmail.com>
2026-04-25 01:15:49 +0300

    Main Menu & Logo & Icons
    - The logo now appears in the main menu.
    - Added more than 7.000 land, air, and naval vehicle/equipment icons.

--- common/defines/01_DH.lua
+++ common/defines/01_DH.lua
@@ -1,3 +1,7 @@
+
+--Graphical Changes
+
 NDefines_Graphics.NGraphics.COUNTRY_COLOR_HUE_MODIFIER = 0.0 -- 0.0
 NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.8 -- 0.6
 NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.75 -- 0.8
+
